61198695416785cfc845166c4118df019c12c4c1
cirill98 <100434317+cirill98@users.noreply.github.com>
2022-09-16 18:57:36 +0500

    Если условие совпадает, записываем элемент во второй массив

--- work/Program.cs
+++ work/Program.cs
@@ -5,6 +5,8 @@ for (int i = 0; i < array1.Length; i++)
     int count = 0;
     if (array1[i].Length <= 3)
     {
-
+        array2[count] = array1[i];
+        Console.Write($"{array2[count]} ");
+        count++;
     }
 }
